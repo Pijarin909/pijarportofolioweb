e7fe39e86976a11aeb1e6973703c06e1ed82f5b4
Pijar Wening Galih <132967687+Pijarin909@users.noreply.github.com>
2025-11-11 12:07:59 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+// Toggle menu untuk mobile
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
-menu.addEventListener('click', function () {
+menu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
+});
+
+// Animasi ikon hamburger jadi X
+menu.addEventListener('click', () => {
+  menu.classList.toggle('open');
 });
